87010b25af35570305de187d63dcb03403d34cc5
Jennie Chu <jenniechu92@gmail.com>
2016-12-19 04:59:02 +0000

    fixed cd bug with not finding certain directories

--- hsh_cd.c
+++ hsh_cd.c
@@ -1,18 +1,23 @@
 #include "shell.h"
 /**
  * hsh_cd - builtin command hsh_cd, mimics cd
- * @??:
+ * @arg_list: argumenet list given by user, generally gives which directory
+ * to change to
+ * @envp: environemental variable linked list to get current working direcoty
+ * and also to update the variables PWD and OLDPWD
+ * @buf_size: size to create our buffer for path
  */
 int hsh_cd(char **arg_list, env_t *envp, int buf_size)
 {
-	/* bug on . and ..*/
-	char *path, *pwd, *arg1;
-	char buff[buf_size];
+	char *path;
+	char buff[buf_size], pwd[buf_size];
 	int n;
 
-	path = safe_malloc(sizeof(char) * buf_size), memset(path, '\0', buf_size);
-	pwd = safe_malloc(sizeof(char) * buf_size), memset(path,'\0', buf_size);
+	path = safe_malloc(sizeof(char) * buf_size);
+	memset(path, '\0', buf_size);
+	memset(pwd, '\0', buf_size);
 	memset(buff, '\0', buf_size);
+	getcwd(pwd, buf_size);
 	if (arg_list[1] == NULL || (n = strcmp(arg_list[1], "~")) == 0)
 	{
 		path = rm_vname(envp, "HOME=", buf_size);
@@ -25,17 +30,16 @@ int hsh_cd(char **arg_list, env_t *envp, int buf_size)
 	}
 	else
 	{
-		path = rm_vname(envp, "PWD=", buf_size);
-		strcat(path,"/");
-		strcat(path, arg_list[1]);
-		n = chdir(path);
+		getcwd(buff, buf_size);
+		strcat(buff,"/");
+		strcat(buff, arg_list[1]);
+		n = chdir(buff);
 
 	}
 	if (n < 0)
 		printf("Error: no such file or directory\n");
 	else
 	{
-		pwd = rm_vname(envp, "PWD=", buf_size);
 		getcwd(buff, buf_size);
 		update_env(envp, "OLDPWD=", pwd);
 		update_env(envp, "PWD=", buff);
